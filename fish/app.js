fb97c46d21245fb9245688e336c8419e14711a7b
Arnaud Henry <>
2024-04-13 23:31:08 +0100

    click + smile for coin

--- fish/app.js
+++ fish/app.js
@@ -1,8 +1,13 @@
 // TODO show/link to license of face-api.js
 
 const urlParams = new URLSearchParams(window.location.search);
-const showVideo = urlParams.get('showvideo') === 'true';
-const timer_duration = urlParams.get('timer') || 300;
+const show_video = urlParams.get('show_video') === 'true';
+const timer_duration = urlParams.get('timer_duration') || 300;
+const trial_duration = urlParams.get('trial_duration') || 5;
+
+if (timer_duration % trial_duration != 0) {
+    throw new Error('Timer duration should be a multiple of trial duration');
+}
 
 let smileDetected = false;
 let score = 0;
@@ -16,12 +21,17 @@ async function loadModels() {
     // Other models can be loaded similarly if needed
 }
 
+
+const timerElement = document.getElementById('timer');
+const emojiElement = document.getElementById('emoji');
+const coinElement = document.getElementById('coin');
+
 this.video = document.createElement('video');
 this.video.setAttribute('id', 'video');
 this.video.setAttribute('autoplay', 'muted');
 document.body.appendChild(this.video);
 
-if (showVideo) {
+if (show_video) {
     document.getElementById('video').style.display = 'block';
 } else {
     document.getElementById('video').style.display = 'none';
@@ -57,7 +67,7 @@ async function detectFaces() {
         options = new faceapi.TinyFaceDetectorOptions();
         detections = await faceapi.detectAllFaces(video, options).withFaceExpressions();
 
-        console.log('detections', detections);
+        // console.log('detections', detections);
 
 
 
@@ -67,8 +77,15 @@ async function detectFaces() {
             //     console.log("Smile Detected!");
             // }
         // });
-
-        smileDetected = detections.some(detection => detection.expressions.happy > 0.1);
+        if (detections.length > 0) {
+            smileDetected = detections[0].expressions.happy > 0.1;
+            if (smileDetected) {
+                console.log('Smile detected');
+            }
+        } { 
+            // TODO error if no face detected?
+        }
+        // smileDetected = detections.some(detection => detection.expressions.happy > 0.1);
     } else {
         console.log('MODELS NOT LOADED');
     }
@@ -81,40 +98,65 @@ loadModels().then(() => {
     setupWebcam();
 });
 
-let timer_completed = false;
-const start = Date.now();
-let timer = timer_duration;
-const timerElement = document.getElementById('timer');
+const start_time = Date.now();
+const interval = 1000;
+let expected = start_time;
+
+let correct_click = false;
+let correct_face = false;
+
+
 function updateTimer() {
-    let delta = Date.now() - start; // milliseconds elapsed since start
-    let remainingSeconds = Math.max(timer - Math.floor(delta / 1000), 0);
-    let minutes = Math.floor(remainingSeconds / 60);
-    let seconds = remainingSeconds % 60;
+    let drift = Date.now() - expected;
+    let elapsed_milliseconds = Date.now() - start_time;
+    console.log('elapsed_milliseconds', elapsed_milliseconds);
+    let elapsed_seconds = Math.round(elapsed_milliseconds / 1000);
+    console.log('elapsed_seconds', elapsed_seconds);
+    let remaining_seconds = Math.max(timer_duration - elapsed_seconds, 0);
+    let minutes = Math.floor(remaining_seconds / 60);
+    let seconds = remaining_seconds % 60;
     timerElement.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    if (remainingSeconds === 0) {
-        timer_completed = true;
+    // Check if the trial is complete
+    if (remaining_seconds % trial_duration == 0) {
+        // Check if both click and facial expression were correct
+        if (correct_click && correct_face) {
+            score++;
+            document.getElementById('score').textContent = score;
+        // if (correct_click) {
+            // Display gold coin
+            coinElement.style.visibility = 'visible';
+            setTimeout(() => {
+                coinElement.style.visibility = 'hidden';
+            }, 100);
+        }
+        // Reset correct count for the next trial
+        correct_click = false;
+        correct_face = false;
+    }
+    if (remaining_seconds === 0) {
         timerElement.textContent = 'Game Complete!';
-        return;
+    } else {
+        expected += interval;
+        // Self-adjusting timer
+        setTimeout(updateTimer, Math.max(0, interval - drift));
     }
 }
-setInterval(updateTimer, 500);
 updateTimer();
 
 function clickFish() {
-    console.log('last detections', detections[0].expressions);
-    const emojiElement = document.getElementById('emoji');
+    correct_click = true;
+    // console.log('last detections', detections[0].expressions);
     if (smileDetected) {
-        score++;
-        document.getElementById('score').textContent = score;
-        emojiElement.textContent = '🟢'; // Green circle for smile
+        correct_face = true;
+        // emojiElement.textContent = '🟢'; // Green circle for smile
     } else {
-        emojiElement.textContent = '🔴'; // Red circle for no smile
+        // emojiElement.textContent = '🔴'; // Red circle for no smile
     }
 
-    // Reset to fish emoji after some time
-    setTimeout(() => {
-        emojiElement.textContent = '🐟';
-    }, 500);
+    // // Reset to fish emoji after some time
+    // setTimeout(() => {
+    //     emojiElement.textContent = '🐟';
+    // }, 500);
 }
 document.getElementById('emoji').addEventListener('click', clickFish);
 
